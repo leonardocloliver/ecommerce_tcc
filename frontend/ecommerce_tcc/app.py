7951c1aaaea6f01fab5b5399dec079e0c32b2a88
Leonardo <leonardocloliver@gmail.com>
2023-03-06 18:10:29 -0300

    front

--- frontend/ecommerce_tcc/app.py
+++ frontend/ecommerce_tcc/app.py
@@ -1,5 +1,5 @@
-import requests
-from flask import Flask, render_template
+import requests 
+from flask import Flask, render_template, request, url_for, redirect
 from .config import get_backend_url
 
 
@@ -22,3 +22,24 @@ def warehouse():
 def cart():
     return render_template('cart.html')
 
+# TO DO: VALIDAR O ERRO
+@app.route('/product', methods=('GET', 'POST'))
+def product():
+    if request.method == 'GET':
+        return render_template('new_product.html')
+    elif request.method == 'POST':
+        new_product = {}
+        new_product["sku"] = request.form['sku']
+        new_product["desc"] = request.form['description']
+        new_product["photo"] = request.form['photo']
+        new_product["available_qty"] = int(request.form['quantity'])
+        new_product["price"] = request.form['price']
+        response = requests.post(BACKEND_URL + "/v1/api/products/", json=new_product)
+        return redirect(url_for('warehouse'))
+
+@app.route('/product/<int:pid>', methods=('DELETE',))
+def delete_product(pid):
+    response = requests.delete(BACKEND_URL + f"/v1/api/products/{pid}")
+    return redirect(url_for('warehouse'))
+    
+    
